41f5098af34f0907b631781ec0492f15b7f5aa5c
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-12-21 21:57:56 +0800

    feat: codes of controllers, routers, and models

---
+++ api/users/user.service.js
@@ -0,0 +1,92 @@
+const pool = require("../../config/database");
+
+module.exports = {
+  create: (data, callBack) => {
+    pool.query(
+      `INSERT INTO restapi.registration (firstName, lastName, gender, email, password, number) 
+                VALUES(?,?,?,?,?,?)`,
+      [
+        data.first_name,
+        data.last_name,
+        data.gender,
+        data.email,
+        data.password,
+        data.number
+      ],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results);
+      }
+    );
+  },
+  getUserByUserEmail: (email, callBack) => {
+    pool.query(
+      `SELECT * FROM restapi.registration where email = ?`,
+      [email],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  getUserByUserId: (id, callBack) => {
+    pool.query(
+      `SELECT id,firstName,lastName,gender,email,number FROM restapi.registration where id = ?`,
+      [id],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  getUsers: callBack => {
+    pool.query(
+      `SELECT id,firstName,lastName,gender,email,number FROM restapi.registration`,
+      [],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results);
+      }
+    );
+  },
+  updateUser: (data, callBack) => {
+    pool.query(
+      `UPDATE restapi.registration SET firstName=?, lastName=?, gender=?, email=?, password=?, number=? where id = ?`,
+      [
+        data.first_name,
+        data.last_name,
+        data.gender,
+        data.email,
+        data.password,
+        data.number,
+        data.id
+      ],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  deleteUser: (data, callBack) => {
+    pool.query(
+      `DELETE FROM restapi.registration WHERE id = ?`,
+      [data.id],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  }
+};
